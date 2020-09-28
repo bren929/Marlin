c6a655e1d412865201328d1d4c1c06b8be41d71d
kamikazeBDM <35385974+kamikazeBDM@users.noreply.github.com>
2020-09-28 09:28:25 -0700

    Update Configuration.h
    Default BED PID settings - Ender 3 Pro

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -524,6 +524,7 @@
 #define HEATER_7_MAXTEMP 275
 #define BED_MAXTEMP      150
 
+
 //===========================================================================
 //============================= PID Settings ================================
 //===========================================================================
@@ -600,15 +601,13 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
-<<<<<<< Updated upstream
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  //#define DEFAULT_bedKp 10.00
+  //#define DEFAULT_bedKi .023
+  //#define DEFAULT_bedKd 305.4
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
-=======
 
   // Stock Creality Ender 3 pro Heated Bed
   // 09/27/2020
@@ -623,7 +622,6 @@
 
 
   // FIND YOUR OWN: "M303 E-1 C8 S90 U1" to run autotune on the bed at 90 degreesC for 8 cycles.
->>>>>>> Stashed changes
 #endif // PIDTEMPBED
 
 #if EITHER(PIDTEMP, PIDTEMPBED)
