fe93eebd87eb7eaf9a8ade5f26ed0bcb03aeba29
kamikazeBDM <35385974+kamikazeBDM@users.noreply.github.com>
2020-10-26 11:23:57 -0700

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -26,7 +26,7 @@
  * Marlin bugfix-2.0.x
  * Compiled version identifier
  ************************************/
-#define COMPILE_VERSION "36" 
+#define COMPILE_VERSION "37" 
 
 #define SHORT_BUILD_VERSION "bugfix-2.0.7.2-" COMPILE_VERSION
 #define STRING_CONFIG_H_AUTHOR "BDM"                // Who made the changes.
@@ -36,7 +36,9 @@
 /*******************************************************************************************************
  * Vx  mm/dd/YY HH:mm 24HR
  * 
- * V36  10/20/21 13:55
+ * V37  10/20/xx xx:xx
+ *
+ * V36  10/20/21 14:05
  * 		firmware sync of settings
  *      upstream merges
  *	
